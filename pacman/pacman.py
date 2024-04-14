48e975093cf5cb56da660f2c11d9af354b2e2519
jacksonrodrigodev <jackson_techno2@hotmail.com>
2024-04-13 21:40:11 -0300

    Ajuste para deixar apenas o arquivo do jogo

--- pacman/pacman.py
+++ pacman/pacman.py
@@ -2,36 +2,178 @@ import pygame
 
 AMARELO = (255,255,0)
 PRETO = (0,0,0)
-LARGURA_TELA = 600
-ALTURA_TELA = 480
+AZUL = (0,0,255)
+LARGURA_TELA = 800
+ALTURA_TELA = 600
 RUNNING = True
-RAIO = 30
-x = 10
-y = 10
-velocidade = 0.1
-vel_x = velocidade
-vel_y = velocidade
+VELOCIDADE = 1
 pygame.init()
 
 tela = pygame.display.set_mode((LARGURA_TELA,ALTURA_TELA),0)
 
-while RUNNING:
-    #Calcular as regras 
-    x = x  + vel_x
-    y = y + vel_y
-    if x + RAIO > LARGURA_TELA:
-        vel_x = -velocidade
-    if x - RAIO < 0:
-        vel_x = velocidade
-    if y + RAIO > ALTURA_TELA:
-        vel_y = -velocidade
-    if y - RAIO < 0:
-        vel_y = velocidade
-    #Pintar 
-    tela.fill(PRETO)
-    pygame.draw.circle(tela, AMARELO, (int(x),int(y)), RAIO, 0)
-    pygame.display.update()
-    #Evento
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            exit()
+fonte = pygame.font.SysFont("arial",24,True,False)
+
+class Cenario:
+    def __init__(self,tamanho,pac):
+        self.pacman = pac
+        self.tamanho = tamanho
+        self.pontos = 0
+        self.matriz = [
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 0, 0, 0, 0, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+        ]
+    
+    def pintar_pontos(self,tela):
+        pontos_x = self.tamanho * 30
+        img_pontos = fonte.render(f"Score: {self.pontos}",True,AMARELO)
+        tela.blit(img_pontos,(pontos_x,50))
+
+    def pintar_linha(self,tela,numero_linha, linha):
+        for numero_coluna, coluna in enumerate(linha):
+            x= numero_coluna * self.tamanho
+            y= numero_linha * self.tamanho
+            half = self.tamanho // 2
+            cor = PRETO
+            if coluna == 2:
+                cor = AZUL
+            pygame.draw.rect(tela,cor,(x,y,self.tamanho,self.tamanho),0)
+            if coluna ==1:
+                pygame.draw.circle(tela,AMARELO,(x+half,y+half),self.tamanho//10,0)
+
+    
+    def pintar(self,tela):
+        for numero_linha, linha in enumerate(self.matriz):
+            self.pintar_linha(tela,numero_linha,linha)
+        self.pintar_pontos(tela)
+    
+    def calcular_regras(self):
+        col = self.pacman.coluna_intensao
+        lin = self.pacman.linha_intensao
+        if 0 <= col < 28 and 0 <= lin < 29:
+            if self.matriz[lin][col] != 2:
+                self.pacman.aceitar_movimento() 
+                if self.matriz[lin][col] == 1:
+                    self.pontos += 1 
+                    self.matriz[lin][col] = 0
+
+
+class Pacman:
+    def __init__(self,tamanho):
+        self.coluna = 1
+        self.linha = 1
+        self.centro_x = 400
+        self.centro_y = 300
+        self.vel_x = 0
+        self.vel_y = 0
+        self.tamanho = tamanho
+        self.raio = int(self.tamanho / 2)
+        self.coluna_intensao = self.coluna
+        self.linha_intensao = self.linha
+
+    def calcular_regras(self):
+        self.coluna_intensao = self.coluna + self.vel_x
+        self.linha_intensao = self.linha + self.vel_y
+        self.centro_x = int(self.coluna * self.tamanho + self.raio)
+        self.centro_y = int(self.linha * self.tamanho + self.raio)
+
+    def pintar(self, tela):
+        #Desenha corpo pacman
+        pygame.draw.circle(tela,AMARELO,(self.centro_x,self.centro_y),self.raio,0)
+        
+        #Desenho boca pacman
+        canto_boca = (self.centro_x,self.centro_y)
+        labio_superior = (self.centro_x + self.raio, self.centro_y - self.raio)
+        labio_inferior = (self.centro_x + self.raio, self.centro_y)
+        pontos = [canto_boca,labio_superior,labio_inferior]
+        pygame.draw.polygon(tela,PRETO,pontos,0)
+
+        #desenha olho pacman
+        olho_x = int(self.centro_x + self.raio / 3)
+        olho_y = int(self.centro_y - self.raio * 0.70)
+        olho_raio = int(self.raio / 10)
+        pygame.draw.circle(tela,PRETO,(olho_x,olho_y), olho_raio,0)
+    
+    def processar_eventos(self,eventos):
+        for e in eventos:
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_RIGHT:
+                    self.vel_x = VELOCIDADE
+                elif e.key == pygame.K_LEFT:
+                    self.vel_x = -VELOCIDADE
+                elif e.key == pygame.K_UP:
+                    self.vel_y = -VELOCIDADE
+                elif e.key == pygame.K_DOWN:
+                    self.vel_y = VELOCIDADE
+            elif e.type == pygame.KEYUP:
+                if e.key == pygame.K_RIGHT:
+                    self.vel_x = 0
+                elif e.key == pygame.K_LEFT:
+                    self.vel_x = 0
+                elif e.key == pygame.K_UP:
+                    self.vel_y = 0
+                elif e.key == pygame.K_DOWN:
+                    self.vel_y = 0
+    
+    def processar_eventos_mouse(self,eventos):
+        delay = 100
+        for e in eventos:
+            if e.type == pygame.MOUSEMOTION:
+                mouse_x, mouse_y = e.pos
+                self.coluna = (mouse_x - self.centro_x) / delay
+                self.linha = (mouse_y - self.centro_y) / delay
+    
+    def aceitar_movimento(self):
+        self.linha = self.linha_intensao
+        self.coluna = self.coluna_intensao
+
+
+
+if __name__ == "__main__":
+    size = ALTURA_TELA//30
+    pacman = Pacman(size)
+    cenario = Cenario(size,pacman)
+    while RUNNING:
+        #Calcular regras
+        pacman.calcular_regras()
+        cenario.calcular_regras()
+        
+        #Pintar tela
+        tela.fill(PRETO)
+        cenario.pintar(tela)
+        pacman.pintar(tela)
+        pygame.display.update()
+        pygame.time.delay(100)
+
+        #Capturar os evento
+        eventos = pygame.event.get()
+        for e in eventos:
+            if e.type == pygame.QUIT:
+                exit()
+        pacman.processar_eventos(eventos)
